8a8969d7f2c3842f40641c495eb92e1726400ee6
Tyronne Jeong <maurois@nate.com>
2022-02-05 23:41:43 +0900

    Initial commit

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')), # urls 파일을 include 시킨다.
+    path('polls/', include('polls.urls')), # include urls.py file in apps
     path('admin/', admin.site.urls),
 ]
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# decalred ORM Information (Object Relation Mapping)
+class Question(models.Model):
+    question_test = models.CharField(max_length=200)
+    pub_data = models.DateTimeField('date published')
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # FK 연결
+    
+    choice_text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)    
