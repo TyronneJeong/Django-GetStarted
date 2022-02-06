f5b441900220801e73997fb2c487b7e92e0a5278
Tyronne Jeong <maurois@nate.com>
2022-02-07 01:48:57 +0900

    Initial commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,28 @@
 from django.urls import path
 from . import views
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 
 urlpatterns = [
     # /blog/home
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    
+    # /blog/detail
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    
+    # /blog/new
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    
+    # /blog/update
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    
+    # /blog/delete
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     
     # /blog/about
     path('about/', views.about, name='blog-about'),
